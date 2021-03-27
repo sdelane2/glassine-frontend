9ac880a26fd44130f388004ea118f1c87dac88fc
sdelane2 <seanmartindelaney@gmail.com>
2021-03-27 16:13:00 -0400

    jss navbar styling

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,16 +1,44 @@
 import React from 'react'
+import {createUseStyles} from 'react-jss'
 
 
 
 
 function NavBar() {
 
+    const useStyles = createUseStyles({
+        myButton: {
+          color: 'green',
+          margin: {
+            // jss-plugin-expand gives more readable syntax
+            top: 5, // jss-plugin-default-unit makes this 5px
+            right: 0,
+            bottom: 0,
+            left: '1rem'
+          },
+          '& span': {
+            // jss-plugin-nested applies this to a child span
+            fontWeight: 'bold' // jss-plugin-camel-case turns this into 'font-weight'
+          }
+        },
+        myLabel: {
+          fontStyle: 'italic'
+        }
+      })
 
+      const Button = ({children}) => {
+        const classes = useStyles()
+        return (
+          <button className={classes.myButton}>
+            <span className={classes.myLabel}>{children}</span>
+          </button>
+        )
+      }
     
 
     return(
         <div>
-            
+            <Button>submit</Button>
         </div>
     )
 
