249ea14706dc5ea5f644d522787a489ce01af05f
sdelane2 <seanmartindelaney@gmail.com>
2021-04-11 17:49:27 -0400

    added fonts

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react'
 
 const hello = () => (
-    <div class="background-image" style={{ backgroundImage: "url(/handling-background-image.jpg)"}}>
+    <div class="background-image">
         <div class="text-container">
             <h1> Help when you need it</h1>
+            <p>Get help from our professional and trusted art handlers</p>
+            <input></input>
         </div>
     </div>
 );
