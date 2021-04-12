089861131eeb2c6306af72f5b3024da24cb67534
sdelane2 <seanmartindelaney@gmail.com>
2021-04-12 19:12:30 -0400

    styled forms

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,13 +4,13 @@
 const Login = props => {
 
     return (
-        
+        <div class="login-page">
             <div class="login-panel">
                 <h1>glassine</h1>
                 <a href='/signup-form'><button id="sign-up"> Sign Up</button></a>
                 <a href='/login-form'><button id="log-in">Log in</button></a>
             </div>
-        
+        </div>
 
     );
 
--- src/components/login/signupForm.js
+++ src/components/login/signupForm.js
@@ -1,7 +1,17 @@
+import react, {useState, useEffect} from 'react'
+
 const SignupForm = (props) => {
+
+    const [user, setUser] = useState({})
+
     return(
-    <div>
-        in the signup form
+    <div class="login-panel">
+        <h1>glassine</h1>
+        <form>
+            <input type="email" class="signup-form" placeholder="Email address"></input>
+            <input type="password" class="signup-form" placeholder="Password"></input>
+            <button id="sign-up">Create Account</button>
+        </form>
     </div>
     );
 };
