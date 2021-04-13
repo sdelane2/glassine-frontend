d567869ce59622d9f10d292c9cfd615dc7730f0c
sdelane2 <seanmartindelaney@gmail.com>
2021-04-13 17:11:25 -0400

    login and signup submit handlers

--- src/components/login/loginForm.js
+++ src/components/login/loginForm.js
@@ -1,6 +1,53 @@
+import react, {useState} from 'react'
+
 const LoginForm = (props) => {
-    return(<div>
-        in the login form
+    const [email, setEmailInput] = useState('');
+    const [password, setPasswordInput] = useState('');
+
+    
+
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:3000/api/login", {
+            method: "POST", 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(email, password)
+        })
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+
+
+    return(
+    <div class="login-panel">
+        <h1>glassine</h1>
+        <form onSubmit={submitHandler}>
+            <input 
+                type="email" 
+                class="signup-form" 
+                placeholder="Email address" 
+                value={email}
+                onChange={event => {
+                    setEmailInput(event.target.value);
+                }}>
+            </input>
+            <input 
+                type="password" 
+                class="signup-form" 
+                placeholder="Password"
+                value={password}
+                onChange={event => {
+                    setPasswordInput(event.target.value);
+                }}>
+            </input>
+            <button id="sign-up">Log in</button>
+        </form>
     </div>
     );
 };
--- src/components/login/signupForm.js
+++ src/components/login/signupForm.js
@@ -2,14 +2,53 @@ import react, {useState, useEffect} from 'react'
 
 const SignupForm = (props) => {
 
-    const [user, setUser] = useState({})
+    const [email, setEmailInput] = useState('');
+    const [password, setPasswordInput] = useState('');
+
+    const userSignUpData = {
+        email: email,
+        password: password
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        fetch("http://localhost:3000/api/signup", {
+            method: "POST", 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userSignUpData)
+        })
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
+
+
 
     return(
     <div class="login-panel">
         <h1>glassine</h1>
-        <form>
-            <input type="email" class="signup-form" placeholder="Email address"></input>
-            <input type="password" class="signup-form" placeholder="Password"></input>
+        <form onSubmit={submitHandler}>
+            <input 
+                type="email" 
+                class="signup-form" 
+                placeholder="Email address" 
+                value={email}
+                onChange={event => {
+                    setEmailInput(event.target.value);
+                }}>
+            </input>
+            <input 
+                type="password" 
+                class="signup-form" 
+                placeholder="Password"
+                value={password}
+                onChange={event => {
+                    setPasswordInput(event.target.value);
+                }}>
+            </input>
             <button id="sign-up">Create Account</button>
         </form>
     </div>
